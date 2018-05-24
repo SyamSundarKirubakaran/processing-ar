96f89e52f6e0c1498c7e742a256da9770cbbb028
Syam Sundar K <k.syamsundar21@gmail.com>
2018-05-24 22:37:51 +0530

    camera permission real-time

--- src/processing/ar/PAR.java
+++ src/processing/ar/PAR.java
@@ -1,9 +1,13 @@
 package processing.ar;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.opengl.GLSurfaceView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.Session;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
@@ -15,7 +19,7 @@ import processing.core.PApplet;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class PAR implements GLSurfaceView.Renderer{
+public class PAR implements GLSurfaceView.Renderer {
 
     //checking if the import statement works as expected.
 
@@ -30,6 +34,9 @@ public class PAR implements GLSurfaceView.Renderer{
     private static String C_EXCEPT_UPDATE_APP = "Please update this app";
     private static String C_DEVICE = "This device does not support AR";
 
+    private static final int CAMERA_PERMISSION_CODE = 0;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+
     //end of check
 
     public PAR(PApplet sketch) {
@@ -43,35 +50,38 @@ public class PAR implements GLSurfaceView.Renderer{
                         message(T_ALERT_MESSAGE, C_NOT_SUPPORTED);
                         return;
                     case INSTALLED:
-                        message(T_PROMPT_MESSAGE,C_SUPPORTED);
+//                        message(T_PROMPT_MESSAGE,C_SUPPORTED);
                         break;
                 }
                 session = new Session(/* context= */ sketch.getContext());
             } catch (UnavailableArcoreNotInstalledException
                     | UnavailableUserDeclinedInstallationException e) {
-                message = C_EXCEPT_INSTALL;
-                exception = e.toString();
+//                message = C_EXCEPT_INSTALL;
+//                exception = e.toString();
             } catch (UnavailableApkTooOldException e) {
-                message = C_EXCEPT_UPDATE_SDK;
-                exception = e.toString();
+//                message = C_EXCEPT_UPDATE_SDK;
+//                exception = e.toString();
             } catch (UnavailableSdkTooOldException e) {
-                message = C_EXCEPT_UPDATE_APP;
-                exception = e.toString();
+//                message = C_EXCEPT_UPDATE_APP;
+//                exception = e.toString();
             } catch (Exception e) {
-                message = C_DEVICE;
-                exception = e.toString();
+//                message = C_DEVICE;
+//                exception = e.toString();
             }
 
-            if(message != null){
-                message(T_ALERT_MESSAGE,message+"--"+exception);
-            }
-
-            // Create default config and check if supported.
-//            Config config = new Config(session);
-//            if (!session.isSupported(config)) {
-//                message("3", "This Device doesn't support AR.!");
+//            if(message != null){
+//                message(T_ALERT_MESSAGE,message+"--"+exception);
 //            }
-//            session.configure(config);
+
+
+            if (!hasCameraPermission(sketch.getActivity())) {
+                requestCameraPermission(sketch.getActivity());
+//                if (!shouldShowRequestPermissionRationale(sketch.getActivity())) {
+//                     Permission denied with checking "Do not ask again".
+//                    launchPermissionSettings(sketch.getActivity());
+//                }
+            }
+
         }
     }
 
@@ -110,5 +120,29 @@ public class PAR implements GLSurfaceView.Renderer{
     public void onDrawFrame(GL10 gl) {
 
     }
+
+    public static boolean hasCameraPermission(Activity activity) {
+        return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /** Check to see we have the necessary permissions for this app, and ask for them if we don't. */
+    public static void requestCameraPermission(Activity activity) {
+        ActivityCompat.requestPermissions(
+                activity, new String[] {CAMERA_PERMISSION}, CAMERA_PERMISSION_CODE);
+    }
+
+//    /** Check to see if we need to show the rationale for this permission. */
+//    public static boolean shouldShowRequestPermissionRationale(Activity activity) {
+//        return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION);
+//    }
+//
+//    /** Launch Application Setting to grant permission. */
+//    public static void launchPermissionSettings(Activity activity) {
+//        Intent intent = new Intent();
+//        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//        intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+//        activity.startActivity(intent);
+//    }
 
 }
