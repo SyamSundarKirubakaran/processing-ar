869d0e32b808b9fa9c420e984ffdf57dbc0dccda
Syam Sundar K <k.syamsundar21@gmail.com>
2018-06-05 19:40:29 +0530

    build file - shader updates

--- src/processing/ar/PGraphicsARView.java
+++ src/processing/ar/PGraphicsARView.java
@@ -1,8 +1,16 @@
 package processing.ar;
 
+import android.view.SurfaceHolder;
+import processing.android.AppComponent;
+import processing.core.PSurface;
+
 public class PGraphicsARView extends PGraphicsAR {
 
-    public PGraphicsARView() {
+    public PGraphicsARView() {}
 
+    @Override
+    public PSurface createSurface(AppComponent appComponent, SurfaceHolder surfaceHolder, boolean b) {
+//        if (b) pgl.resetFBOLayer();
+        return new PSurfaceAR(this,appComponent,surfaceHolder);
     }
 }
