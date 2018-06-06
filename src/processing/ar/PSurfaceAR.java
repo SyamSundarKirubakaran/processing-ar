a172de1ff7924626385c9fcb21cee86528c34aa6
Syam Sundar K <k.syamsundar21@gmail.com>
2018-06-06 21:05:25 +0530

    surface start

--- src/processing/ar/PSurfaceAR.java
+++ src/processing/ar/PSurfaceAR.java
@@ -1,14 +1,30 @@
 package processing.ar;
 
 import android.view.SurfaceHolder;
+import com.google.ar.core.Session;
 import processing.android.AppComponent;
+import processing.ar.render.Background;
+import processing.ar.render.RotationHandler;
 import processing.core.PGraphics;
+import processing.opengl.PGLES;
+import processing.opengl.PGraphicsOpenGL;
 import processing.opengl.PSurfaceGLES;
 
 public class PSurfaceAR extends PSurfaceGLES {
 
-    public PSurfaceAR(PGraphics pGraphics, AppComponent appComponent, SurfaceHolder surfaceHolder) {
-        super(pGraphics, appComponent, surfaceHolder);
-    }
+    private SurfaceViewGLES gl;
 
+    private Session session;
+    private RotationHandler displayRotationHelper;
+
+    private final Background backgroundRenderer = new Background();
+
+
+    public PSurfaceAR(PGraphics graphics, AppComponent appComponent, SurfaceHolder surfaceHolder) {
+        super(graphics,appComponent,surfaceHolder);
+        this.sketch = graphics.parent;
+        this.graphics = graphics;
+        this.component = appComponent;
+        this.pgl = (PGLES)((PGraphicsOpenGL)graphics).pgl;
+    }
 }
